aa1c3619639f53a718d77104069e375850e63f5c
awu68 <awu68@ucsc.edu>
2023-05-01 19:12:30 -0700

    back

--- iPad-Calc/iPad-Calc/HorizontalViewObjects/ButtonPadHorz.swift
+++ iPad-Calc/iPad-Calc/HorizontalViewObjects/ButtonPadHorz.swift
@@ -6,7 +6,10 @@
 //
 
 import SwiftUI
-
+import Darwin
+let e = Darwin.M_E
+let pi = Double.pi
+import Foundation
 
 struct ButtonPadHorz: View {
     @State private var currentNumber: String = "0"
@@ -14,6 +17,7 @@ struct ButtonPadHorz: View {
     @State private var storedNumber: Double = 0
     @State private var operation: Operation = .none
     @State private var isDoingOp = false
+    @State private var angle = "DEG"
     
     let buttons = [
         ["(", ")", "mc", "m+", "m-", "mr", "AC", "+/_", "%", "÷"],
@@ -77,6 +81,61 @@ struct ButtonPadHorz: View {
             performOperation(.divide)
         case "=":
             performEquals()
+        case "x^2":
+            powerOf(x: Double(currentNumber)!, y: 2)
+        case "x^3":
+            powerOf(x: Double(currentNumber)!, y: 3)
+        case "x^y":
+            powerOf(x: storedNumber, y: Double(currentNumber)!)
+        case "e^x":
+            powerOf(x: e, y: Double(currentNumber)!)
+        case "10^x":
+            powerOf(x: 10, y: Double(currentNumber)!)
+        case "1/x":
+            inv(x: Double(currentNumber)!)
+        case "sqrt":
+            powerOf(x: Double(currentNumber)!, y: 1/2)
+        case "cubrt":
+            powerOf(x: Double(currentNumber)!, y: 1/3)
+        case "yrt":
+            powerOf(x: Double(currentNumber)!, y: 1/storedNumber)
+        case "ln":
+            logfn(x: e, y: Double(currentNumber)!)
+        case "log10":
+            logfn(x: Double(currentNumber)!, y: 10)
+        case "x!":
+            fact(x: Int(currentNumber)!)
+        case "sin":
+            performTrig(x: Double(currentNumber)!, y: "sin")
+        case "cos":
+            performTrig(x: Double(currentNumber)!, y: "cos")
+        case "tan":
+            performTrig(x: Double(currentNumber)!, y: "tan")
+        case "e":
+            currentNumber = String(e)
+            displayNumber = currentNumber
+        /* case "EE":
+            currentNumber = String(e)
+            displayNumber = currentNumber */
+        case "Rad":
+            if(self.angle == "RAD") {
+                self.angle = "DEG"
+            }
+            else {
+                self.angle = "RAD"
+            }
+        case "sinh":
+            performTrig(x: Double(currentNumber)!, y: "sinh")
+        case "cosh":
+            performTrig(x: Double(currentNumber)!, y: "cosh")
+        case "tanh":
+            performTrig(x: Double(currentNumber)!, y: "tanh")
+        case "pi":
+            currentNumber = String(pi)
+            displayNumber = currentNumber
+        case "Rand":
+            currentNumber = String(Double(arc4random()))
+            displayNumber = currentNumber
         default:
             if let number = Int(button) {
                 appendNumber(number)
@@ -85,7 +144,6 @@ struct ButtonPadHorz: View {
             }
         }
     }
-    
     private func appendNumber(_ number: Int) {
         if currentNumber == "0" {
             currentNumber = String(number)
@@ -130,6 +188,52 @@ struct ButtonPadHorz: View {
         storedNumber = Double(currentNumber)!
         currentNumber = "0"
         self.isDoingOp = true
+    }
+    private func powerOf(x: Double, y: Double) {
+        currentNumber = String(pow(x, y))
+        displayNumber = currentNumber
+    }
+    private func inv(x: Double) {
+        currentNumber = String(1/x)
+        displayNumber = currentNumber
+    }
+    private func logfn(x: Double, y: Double){
+        let current = log(x)/log(y)
+        currentNumber = String(current)
+        displayNumber = currentNumber
+    }
+    private func fact(x: Int){
+        var count = x
+        var current = 1
+        if (x != 0) {
+            while(count > 0) {
+                current *= count
+                count -= 1
+            }
+        }
+        currentNumber = String(current)
+        displayNumber = currentNumber
+    }
+    private func performTrig(x: Double, y: String) {
+        var current = 0.0
+        switch y {
+            case "sin" :
+                current = sin(x)
+            case "cos" :
+                current = cos(x)
+            case "tan" :
+                current = tan(x)
+            case "sinh" :
+                current = sinh(x)
+            case "cosh" :
+                current = cosh(x)
+            case "tanh" :
+                current = tanh(x)
+            default:
+                current = 0.0
+        }
+        currentNumber = String(current)
+        displayNumber = currentNumber
     }
     private func clearAll() {
         self.currentNumber = "0"
